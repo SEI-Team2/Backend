dada4f64709517cff1d0c5a90f5d467f46628f89
HAN SEUNG JIN <82820233+HAN-SEUNG-JIN@users.noreply.github.com>
2024-06-03 19:50:03 +0900

    Update rentals.py

--- rentals.py
+++ rentals.py
@@ -14,8 +14,8 @@ rentals_bp = Blueprint('rentals', __name__)
 def rentals_datetime():
     current_userid = get_jwt_identity()
     data = request.json
-    kst = ZoneInfo("Asia/Seoul")
-    date = datetime.utcnow(kst)
+    kst = pytz.timezone("Asia/Seoul")
+    date = datetime.now(kst)
     return jsonify({'rentals' : date.strftime('%Y-%m-%d %H:%M:%S')), 200
 
 
